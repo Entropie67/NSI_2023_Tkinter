635ed73975875853ffab6e4dd35d307801f9e45f
Entropie67 <olivier.elophe@gmail.com>
2022-11-24 14:19:58 +0100

    version 2

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ root = tk.Tk()
 message_clair = tk.Frame()
 message_clair.grid(column=0, row=0, columnspan=6, rowspan=6)
 titre = tk.Label(message_clair, text="Message en clair")
-titre.grid(column=3, row=0, columnspan=3)
+titre.grid(column=0, row=0, columnspan=3)
+saisie = tk.Text(message_clair)
+saisie.grid(column=0, row=1, columnspan=3)
+bouton = tk.Button(message_clair, text="Chiffrer")
+bouton.grid(column=0, row=4, columnspan=6)
 
+# Partie message chiffré.
 message_chiffre = tk.Frame()
 message_chiffre.grid(column=7, row=0, columnspan=6, rowspan=6)
 titre = tk.Label(message_chiffre, text="Message chiffré")
-titre.grid(column=3, row=0, columnspan=3)
+titre.grid(column=0, row=0, columnspan=3)
+saisie2 = tk.Text(message_chiffre)
+saisie2.grid(column=0, row=1, columnspan=3)
+bouton2 = tk.Button(message_chiffre, text="Déchiffrer")
+bouton2.grid(column=0, row=4, columnspan=6)
 
-saisie = tk.Entry()
-
-
-bouton = tk.Button(text="zone de texte")
-
-bouton2 = tk.Button(text="truc")
 
 
 obj = tk.Label(text="Mon texte")
