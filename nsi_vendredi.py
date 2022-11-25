bfd2b30b6a4080d5991ef4ccd0a6a4c67ad064c7
Entropie67 <olivier.elophe@gmail.com>
2022-11-25 11:07:51 +0100

    Ajout des actions avec déchiffrement

--- nsi_vendredi.py
+++ nsi_vendredi.py
@@ -1,11 +1,12 @@
 # http://www.xavierdupre.fr/app/teachpyx/helpsphinx/c_gui/tkinter.html
 import tkinter as tk
-def coder():
-    f = entree.get()
-    sortie.set("#"+f+"#")
-    print(f)
-def decoder():
-    pass
+
+def action():
+    message = entree.get()
+    sortie.set(coder(message, 3))
+    
+def coder(f, cle):
+    return "".join([chr(ord(l)+cle) for l in list(f)])
 
 root = tk.Tk()
 entree = tk.StringVar()
@@ -17,7 +18,7 @@ titre1 = tk.Label(f1, text="Chiffrement")
 titre1.grid()
 s = tk.Entry(f1, textvariable=entree)
 s.grid()
-chiffre = tk.Button(f1,command=coder, text="Chiffrement")
+chiffre = tk.Button(f1,command=action, text="Chiffrement")
 chiffre.grid()
 # Partie de droite
 f2 = tk.Frame()
@@ -26,7 +27,7 @@ titre2 = tk.Label(f2, text="Déchiffrement")
 titre2.grid()
 s = tk.Entry(f2, textvariable=sortie)
 s.grid()
-chiffre = tk.Button(f2,command=decoder, text="Déchiffrement")
+chiffre = tk.Button(f2,command=action, text="Déchiffrement")
 chiffre.grid()
 
 root.mainloop()
